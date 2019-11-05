e2454e8fb939a81dc8b8584ec6c535fdf0a9a12f
l0rem <loremdevs@gmail.com>
2019-11-05 13:18:45 +0100

    v0.1 ready to publish

--- phrases/mensa_phrases.py
+++ phrases/mensa_phrases.py
@@ -1,7 +1,2 @@
 mensa_menu_phrase = '<b>Mensa menu for today:</b>'
-<<<<<<< HEAD
 wait_phrase = '<code>Please, wait...</code>'
-=======
-
-wait_phrase = '<code>Please, wait...</code>'
->>>>>>> a4cf8ed5cfcc536095c5018efa7c24eff794e8ab
